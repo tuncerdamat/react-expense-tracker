a6cfe4818986b2475a2a087dbb038da12fe77015
tuncerdamat <tuncer.damat@gmail.com>
2024-02-05 15:45:10 +0100

    AddTransaction component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Header } from "./components/Header"
 import { Balance } from "./components/Balance"
 import { IncomeExpenses } from "./components/IncomeExpenses"
 import { TransactionList } from "./components/TransactionList"
+import { AddTransaction } from "./components/AddTransaction"
 import "./App.css"
 
 function App() {
